6d31a4dc30488fa09d5148631c075cce0ad6e3e4
Min Khant Kyaw <minkhantkyaw.99.phoethar@gmail.com>
2022-01-14 00:29:55 +0630

    Front End Completed

---
+++ JS.js
@@ -0,0 +1,13 @@
+let breakIncrementButton = document.getElementById("break-increment");
+let breakDecrementButton = document.getElementById("break-decrement");
+
+let sessionIncrementButton = document.getElementById("session-decrement");
+let sessionDecrementButton = document.getElementById("session-decrement");
+
+let startStopButton = document.getElementById("start_stop");
+let resetButton = document.getElementById("reset");
+
+let breakLength = document.getElementById("break-length");
+let sessionLength = document.getElementById("session-length");
+
+let timeLeft = document.getElementById("time-left");
