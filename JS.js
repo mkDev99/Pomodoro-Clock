498f3310c5a8fbd828c7ff371ed2f3aa7d11291e
Min Khant Kyaw <minkhantkyaw.99.phoethar@gmail.com>
2022-01-20 21:03:55 +0630

    Start/Stop button worked

--- JS.js
+++ JS.js
@@ -11,3 +11,42 @@ let breakLength = document.getElementById("break-length");
 let sessionLength = document.getElementById("session-length");
 
 let timeLeft = document.getElementById("time-left");
+
+let timer;
+let timerStatus = "begin";
+
+startStopButton.addEventListener("click", () => {
+    
+    if (timerStatus === "begin" || timerStatus === "stopped") {
+        timerStatus = "counting";
+        timer  = setInterval(() => {timeLeft.innerText = decrementTime(timeLeft.innerText);}, 1000);
+    }else if (timerStatus === "counting") {
+        timerStatus = "stopped";
+        clearInterval(timer);
+    }
+   
+})
+
+resetButton.addEventListener("click", () => {
+    console.log('Reset Button Clicked');
+    clearInterval(timer);
+})
+
+function decrementTime (timeString) {
+    let timeDisplay = timeString.split(':');
+    let minuteDisplay = parseInt(timeDisplay[0]);
+    let secondDisplay = parseInt(timeDisplay[1]);
+
+    secondDisplay -= 1;
+
+    if (secondDisplay === -1) {
+        secondDisplay = 59;
+        minuteDisplay -= 1;
+    }
+
+    if (secondDisplay <= 9) {
+        secondDisplay = "0" + secondDisplay;
+    }
+
+    return minuteDisplay + ":" + secondDisplay;
+}
